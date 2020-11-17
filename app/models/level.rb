50ba69a8781741fb50c3905a2ce4ab09ce6431e9
KaladaJumbo <wkalada1@gmail.com>
2020-11-17 16:25:29 -0500

    relationships added

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,2 +1,4 @@
 class Level < ApplicationRecord
+    has_many :questions
+    has_many :users
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ApplicationRecord
+    belongs_to :level
+    has_many :question_tags
+    has_many :tags, through: :question_tags
 end
